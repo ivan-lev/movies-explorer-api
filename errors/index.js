e501471f4acc658afc4afe3bee463bd3cb943675
Ivan Lev <fanatos@mail.ru>
2024-01-23 21:32:48 +0500

    fix: fix name to RightsError variable name

--- errors/index.js
+++ errors/index.js
@@ -1,13 +1,13 @@
 const AuthorizationError = require('./authorization-error');
 const ConflictError = require('./conflict-error');
 const NotFoundError = require('./not-found-error');
-const RigthsError = require('./rights-error');
+const RightsError = require('./rights-error');
 const ValidationError = require('./validation-error');
 
 module.exports = {
   AuthorizationError,
   ConflictError,
   NotFoundError,
-  RigthsError,
+  RightsError,
   ValidationError,
 };
