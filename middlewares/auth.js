dd0f5961035965ee059920ac0359797d94ad4e95
Ivan Lev <fanatos@mail.ru>
2024-01-23 17:45:22 +0500

    feat: add authorization middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const { AuthorizationError } = require('../errors');
+
+const { JWT_SECRET = 'secret-key' } = process.env;
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new AuthorizationError('Необходима авторизация'));
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return next(new AuthorizationError('Необходима авторизация'));
+  }
+
+  req.user = payload;
+  return next();
+};
