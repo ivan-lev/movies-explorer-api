07e1133b8c105203682aced98f039a1477cbf13b
Ivan Lev <fanatos@mail.ru>
2024-01-24 16:19:05 +0500

    fix: change port to 3000 in dev mode

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const { errorHandler } = require('./middlewares/error-handler');
 const { routes } = require('./routes');
 
 const {
-  PORT = 3001,
+  PORT = 3000,
   DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
 } = process.env;
 
